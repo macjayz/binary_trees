ab181566d796401deb4c3e14b7ccac992b5ba750
macjayz <macjayzz1@gmail.com>
2023-06-07 08:03:20 -0700

    new commit

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - a function that measures
+ * the depth of a node in a binary tree
+ *
+ * @tree: node to calculate the depth of
+ *
+ * Return: depth of the node
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+if (tree == NULL)
+{
+return (0);
+}
+
+size_t depth = 0;
+const binary_tree_t *currentNode = tree;
+
+while (currentNode->parent != NULL)
+{
+depth++;
+currentNode = currentNode->parent;
+}
+
+return (depth);
+}
